0182192c8196519b46ac42bb34f871ac66ae608d
kwooshung <kwooshung@qq.com>
2024-02-18 03:11:56 +0800

    🐛 fix(config): Fix the coverage of VITEST

--- configs/vitest.ts
+++ configs/vitest.ts
@@ -3,13 +3,13 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    setupFiles: './vitest-setup.ts',
+    setupFiles: './configs/vitest-setup.ts',
     environment: 'jsdom',
     include: ['src/**/*.test.{ts,tsx}'],
     exclude: ['**/node_modules/**', '**/dist/**'],
     coverage: {
-      include: ['src/icons/*.{ts,tsx}'],
-      exclude: ['**/*.test.{ts,tsx}', '**/node_modules/**', '**/dist/**', '**/*.d.ts*']
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['**/*.test.{ts,tsx}', '**/node_modules/**', '**/dist/**', '**/*.d.ts*', 'src/index.ts']
     }
   }
 });
